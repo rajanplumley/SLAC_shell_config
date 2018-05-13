3a2180b5267e8918d43cc4b71445712a5256a295
rajanplumley <rajanplumley@gmail.com>
2018-05-12 21:44:05 -0700

    added gobak and bak2top funcs for speedy up/down cd

--- functions.sh
+++ functions.sh
@@ -3,6 +3,21 @@
 #  RAJAN FUNCTIONS  #
 #####################
 
+# Takes me back as many directories as specified like goback <num>.  Use back2top to return.
+gobak(){
+    export LASTBACKDIR=$PWD;
+    for ((i=0; i<$1; i++)); do
+	cd ../;
+    done;
+}
+bak2top(){
+    if [[ -n $LASTBACKDIR ]]; then
+	cd $LASTBACKDIR;
+    else
+	echo "No top set.  Must 'goback' first";
+    fi
+}
+
 # Reads back the location from a device's netconfig entry
 locate(){
     netconfig view $1 | grep Location
